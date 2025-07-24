e105dab0baf0c8393144ba4a9d43c219073a041f
GiuliaBrigadoi <giulia.brigadoi@gmail.com>
2025-07-24 14:24:13 +0200

    update

---
+++ chunkdata.R
@@ -0,0 +1,18 @@
+data <- read.csv2("~/Google Drive/Il mio Drive/CUAMM/NICU_Beira_ok.csv")
+
+
+
+```{r}
+data <- read.csv2("~/Google Drive/Il mio Drive/CUAMM/NICU_Beira_ok.csv")
+#| echo: true
+head(data)
+```
+
+```{r}
+#| echo: false
+str(data)
+```
+```{r}
+#| echo: fenced
+summary(data)
+```
